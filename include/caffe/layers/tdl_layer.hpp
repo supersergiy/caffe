5e3f1e47c42b00523ee3494fbf85b22ff4a1381f
Sergiy Popovych <popovych@seung-behir02.pni.Princeton.EDU>
2017-03-28 15:58:48 -0400

    added carcases

---
+++ include/caffe/layers/tdl_layer.hpp
@@ -0,0 +1,41 @@
+#ifndef CAFFE_TDL_LAYER_HPP_
+#define CAFFE_TDL_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+namespace caffe {
+
+template <typename Dtype>
+class TdlLayer : public Layer<Dtype> {
+ public:
+  explicit TdlLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+
+  virtual inline const char* type() const { return "tdl"; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+  virtual inline int MinTopBlobs() const { return 1; }
+  // MAX POOL layers can output an extra top blob for the mask;
+  // others can only output the pooled inputs.
+  virtual inline int MaxTopBlobs() const {
+    return 1;//(this->layer_param_.pooling_param().pool() ==
+            //PoolingParameter_PoolMethod_MAX) ? 2 : 1;
+  }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+};
+
+}  // namespace caffe
+
+#endif  // CAFFE_POOLING_LAYER_HPP_
